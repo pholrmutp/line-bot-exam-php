37f0082a3612f2d5ff5882e8cfa56a74ae25fb33
Pholapat Chinchowrang <48017689+pholrmutp@users.noreply.github.com>
2019-02-27 14:22:59 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,14 +6,14 @@ require "vendor/autoload.php";
 
 $access_token = '+yvfpllsF0x8JZDsNrF+PlMOsXi4rj/SU5TIpVfBuXebs9C7hrEiEoc+Ws9V+W/wZsCyNAZNGkH54W9lpTUZ81H1N/90TRmIama6KDuq682PGdN0gwKgc0BaeoyEPBZ8xh4XJtkZ0RySUdhuMWAFKwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '369c8c5d03cea66d8d167ed377f61598';
+$channelSecret = '369c8c5d03cea66d8d167ed377f61598','Ue4fcf5fa0e1fa60d84433c7614e61b56';
 
 $pushID = 'U3fc5b043ff752d5b78c9b2f5eb093084';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีนัด');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Bot Pholpat');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
