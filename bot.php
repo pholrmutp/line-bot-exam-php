4e53960a44d13a28e7370da17dfcaf46e2929020
Pholapat Chinchowrang <48017689+pholrmutp@users.noreply.github.com>
2019-03-04 14:20:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -67,7 +67,7 @@ if(!is_null($events)){
         case 'text':
             switch ($userMessage) {
                 case "uid":
-                    $textReplyMessage = "UID คุณ ".$replyToken;
+                    $textReplyMessage = "UID คุณ ".$replyToken.$event['events'][0]['userId'];
                     $replyData = new TextMessageBuilder($textReplyMessage);
                     break;
                 case "i":
